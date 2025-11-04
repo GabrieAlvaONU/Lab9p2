8d7363a0a6811275087d809bddc0c56c43c348e2
Gabriel Alva <g-alva@onu.edu>
2025-11-04 02:00:40 -0500

    completed hasDuplicates

--- Lab9p2/Source.cpp
+++ Lab9p2/Source.cpp
@@ -8,12 +8,18 @@ bool isSortedIncreasing(int values[], int size);
 
 bool isSortedDecreasing(int values[], int size);
 
-bool hasAdjacentDuplicants(int values[], int size);
+bool hasAdjacentDuplicates(int values[], int size);
+
+bool hasDuplicates(int values[], int size);
 
 int main() {
 
 	const int SIZE = 6;
 	int numbers[SIZE] = { 1, 2, 2, 4, 6, 7 }; // test
+
+
+
+
 
 	if (isSortedIncreasing(numbers, SIZE)) {
 		cout << "The data are increasing." << endl;
@@ -31,28 +37,37 @@ int main() {
 
 	}
 	cout << endl;
-	if (hasAdjacentDuplicants(numbers, SIZE)) {
-		cout << "The data has duplicates." << endl;
+	if (hasAdjacentDuplicates(numbers, SIZE)) {
+		cout << "The data has adjcaent duplicates." << endl;
 	}
 	else {
-		cout << "The data does not have duplicates." << endl;
+		cout << "The data does not have adjacent duplicates." << endl;
 
+		cout << endl;
+
+		if (hasDuplicates(numbers, SIZE)) {
+			cout << "The data has duplicates." << endl;
+		}
+		else {
+			cout << "The data does not have duplicates." << endl;
+
+		}
 	}
 }
-
 	bool isSortedIncreasing(int values[], int size) {
 
 		for (int i = 0; i < size - 1; i++) {
 
 			if (values[i] > values[i + 1]) {
 				return false;
-			
+
 			}
 			else {
 				return true;
 			}
 		}
 	}
+
 	
 	bool isSortedDecreasing(int values[], int size){
 
@@ -66,13 +81,28 @@ int main() {
 			}
 		}
 	}
-	bool hasAdjacentDuplicants(int values[], int size){
+	bool hasAdjacentDuplicates(int values[], int size){
 		for (int i = 0; i < size - 1; i++) {
 			if (values[i] == values[i + 1]) {
 				return true;
 			}
 			else {
 				return false;
+			}
+		}
+
+
+	}
+	
+	bool hasDuplicates(int values[], int size) {
+		for (int i = 0; i < size - 1; i++) {
+			for (int j = i + 1; j < size; j++) {
+				if (values[i] == values[j]) {
+					return true;
+				}
+				else {
+					return false;
+				}
 			}
 		}
 
